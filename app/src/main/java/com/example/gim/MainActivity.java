c109eb26c20dcfacaa7830b0aab5d86810ebe1a6
DanielChavez16 <79770976+DanielChavez16@users.noreply.github.com>
2021-02-27 18:05:59 -0600

    Comentarios añadidos en el codigo

--- app/src/main/java/com/example/gim/MainActivity.java
+++ app/src/main/java/com/example/gim/MainActivity.java
@@ -20,22 +20,28 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Declaracion de las vistas que se van a utilizar
     EditText txt_email, txt_pass;
     String email, pass;
 
+    //Declaracion del objeto de autenticacion en Firebase
     FirebaseAuth firebaseAuth;
 
+    //Metodo que se ejecuta al crear la interfaz
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Obtencion de las vistas de la interfaz
         txt_email = findViewById(R.id.login_txt_email);
         txt_pass = findViewById(R.id.login_txt_pass);
 
+        //Obtencion de la instancia la autenticacion de Firebase
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
+    //Metodo que redirige a la pantalla de registro y limpia los campos de texto / Se ejecuta al presionar el boton "Regístrate"
     public void redirRegistro(View view) {
         Intent registro = new Intent(this, RegistryActivity.class);
         startActivity(registro);
@@ -43,26 +49,33 @@ public class MainActivity extends AppCompatActivity {
         txt_pass.setText("");
     }
 
+    //Metodo que realiza las validaciones necesarias y redirige al menu principal del cliente/empleado / Se ejecuta al presionar el botion "Iniciar Sesión"
     public void redirMenu(View view) {
+        //Obtencion de los datos escritos en los campos de texto
         email = txt_email.getText().toString();
         pass = txt_pass.getText().toString();
 
+        //Verificacion si los datos escritos son aceptables
         if(validarEmail(email) && !pass.equals("")) {
             firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (!task.isSuccessful()) {
+                        //Error: El usuario no se encuentra en el registro de usuarios de firebase
                         Toast.makeText(MainActivity.this, "No existe usuario", Toast.LENGTH_LONG).show();
                     } else {
+                        //Creacion del objeto "user" que indica si el usuario a validado su correo electronico
                         FirebaseUser user = firebaseAuth.getCurrentUser();
 
                         if (user != null) {
                             if (!user.isEmailVerified()) {
+                                //Error: El usuario existe pero no puede iniciar sesion hasta verificar su correo electronico
                                 Toast.makeText(MainActivity.this, "El correo electronico no esta verificado", Toast.LENGTH_LONG).show();
                             } else {
                                 Toast.makeText(MainActivity.this, "El usuario inicio sesión correctamente", Toast.LENGTH_LONG).show();
                             }
                         } else {
+                            //Error: No se pudo iniciar sesion por razones externas
                             Toast.makeText(MainActivity.this, "No se pudo iniciar sesión", Toast.LENGTH_LONG).show();
                         }
                     }
@@ -70,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         else {
+            //Errores que indican que los datos obtenidos en los campos de texto no son aceptados
             if(!email.equals("") && !validarEmail(email)) {
                 Toast.makeText(this, "Correo no valido", Toast.LENGTH_SHORT).show();
             }
@@ -79,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Metodo booleano que valida el correo electronico introducido
     private boolean validarEmail(String email) {
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(email).matches();
--- app/src/main/java/com/example/gim/RegistryActivity.java
+++ app/src/main/java/com/example/gim/RegistryActivity.java
@@ -23,32 +23,41 @@ import java.util.regex.Pattern;
 
 public class RegistryActivity extends AppCompatActivity {
 
+    //Declaracion de las vistas que se van a utilizar
     EditText txt_name, txt_email, txt_pass, txt_pass2;
     String name, email, password, password2;
 
+    //Declaracion del objeto de referencia de la base de datos
     DatabaseReference databaseReference;
+    //Declatacion del objeto de referencia de la base de datos
     FirebaseAuth firebaseAuth;
 
+    //Metodo que se ejecuta al crear la interfaz
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry);
 
+        //Obtenicion de las vistas de la interfaz
         txt_name = findViewById(R.id.reg_txt_name);
         txt_email = findViewById(R.id.reg_txt_email);
         txt_pass = findViewById(R.id.reg_txt_pass);
         txt_pass2 = findViewById(R.id.reg_txt_pass2);
 
+        //Ejecucion del metodo para inicializar firebase
         inicializarFirebase();
     }
 
+    //Metodo que obtiene la referencia de firebase y obtiene la instancia de autenticacion de firebase
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(this);
         databaseReference = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
+    //Metodo que hace la validacion de los datos de usuario ingresados / Se ejecuta al presionar el boton "Registrarse"
     public void registrar(View view) {
+        //Obtencion de los datos de los campos de texto
         name = String.valueOf(txt_name.getText());
         email = String.valueOf(txt_email.getText());
         password = String.valueOf(txt_pass.getText());
@@ -57,20 +66,25 @@ public class RegistryActivity extends AppCompatActivity {
         if(!name.equals("") && validarEmail(email) && !password.equals("")) {
             if (password.length()>=6 && password.equals(password2)) {
                 try{
+                    //Inicia el metodo iniciarRegistro si las validaciones son correctas
                     iniciarRegistro();
                 } catch(Exception e) {
+                    //Error: No se puede iniciar el registro por razones externas
                     Toast.makeText(this, "Error al registrar usuario", Toast.LENGTH_LONG).show();
                 }
             } else {
                 if(password.length() < 6) {
+                    //Error: modificar la longitud de la contraseña introducida
                     Toast.makeText(this, "Error: La contraseña debe tener mas de 6 caracteres", Toast.LENGTH_LONG).show();
                 }
                 else {
+                    //Error: Las contraseñas introducidas no coinciden
                     Toast.makeText(this, "Error: Confirma tu contraseña", Toast.LENGTH_LONG).show();
                 }
             }
         }
         else {
+            //Errores que indican que los datos obtenidos en los campos de texto no son aceptados
             if(!email.equals("") && !validarEmail(email)) {
                 Toast.makeText(this, "Correo no valido", Toast.LENGTH_SHORT).show();
             }
@@ -80,21 +94,26 @@ public class RegistryActivity extends AppCompatActivity {
         }
     }
 
+    //Metodo que registra los datos introducidos en la base de datos
     private void iniciarRegistro() {
+        //Funcion que registra al usuario en el sistema usando correo y contraseña / Registra los datos del usuario en la base de datos
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
-                    FirebaseUser userAuth = firebaseAuth.getCurrentUser();
-                    userAuth.sendEmailVerification();
+                    //Declaracion del objeto que contiene los datos del usuario recientemente creado en el sistema
+                    FirebaseUser userNew = firebaseAuth.getCurrentUser();
+                    //Funcion que envia al usuario un correo para verificar su correo electronico
+                    userNew.sendEmailVerification();
 
                     Usuario user = new Usuario();
-                    user.setId(userAuth.getUid());
+                    user.setId(userNew.getUid());
                     user.setNombre(name);
                     user.setCorreo(email);
                     user.setPassword(password);
                     user.setMembresia("Prueba");
                     user.setTipo("Cliente");
+                    //Funcion que inserta los datos del usuario en la base de datos
                     databaseReference.child("Usuario").child(user.getId()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
@@ -102,23 +121,27 @@ public class RegistryActivity extends AppCompatActivity {
                                 Toast.makeText(RegistryActivity.this, "Usuario registrado\nVerifique su correo para poder iniciar sesion", Toast.LENGTH_LONG).show();
                             }
                             else {
+                                //Error: Los datos del usuario no se pudieron insetrar en la base de datos
                                 Toast.makeText(RegistryActivity.this, "Error: No se pudo ingresar los datos del usuario", Toast.LENGTH_LONG).show();
                             }
                         }
                     });
                 }
                 else {
+                    //Error: No se pudo crear el usuario por razones externas
                     Toast.makeText(RegistryActivity.this, "Error: No se pudo crear el usuario", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
+    //Metodo booleano que valida el correo electronico introducido
     private boolean validarEmail(String email) {
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(email).matches();
     }
 
+    //Metodo que cierra la actividad actual y regresa al menu de inicio de sesión
     public void regresarLogin(View view) {
         finish();
     }
